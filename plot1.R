157dd22d123b415019c5f695966e8b3dae2b64a3
Josh82 <youngj@hmc.harvard.edu>
2015-12-11 10:30:56 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+powerconsumption<-read.csv.sql("C:/Users/youngj/downloads/household_power_consumption.txt",
+                               sql="select* from file WHERE Date in  ('1/2/2007', '2/2/2007')", header=TRUE, sep=";")
+hist(powerconsumption$Global_active_power, col = "red", main = "Global Active Power", 
+      xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.copy(png, file = "plot1.png")
+dev.off()
